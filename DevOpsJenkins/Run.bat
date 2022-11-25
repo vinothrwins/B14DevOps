65e8e346d2955908e9006554bb8be04f047c0cc5
Vinoth R <vinothrwins@gmail.com>
2022-11-25 06:27:43 +0530

    Adding the project

---
+++ DevOpsJenkins/Run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\vinot\Selenium14\DevOpsJenkins
+mvn clean install
---
+++ DevOpsJenkins/src/main/java/seleniumautomation/DevOpsJenkins/App.java
@@ -0,0 +1,13 @@
+package seleniumautomation.DevOpsJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
